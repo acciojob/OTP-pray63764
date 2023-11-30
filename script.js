1518124c52f2933b9558a3b8707140749b90c462
pray63764 <141926153+pray63764@users.noreply.github.com>
2023-11-30 16:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const codes = document.querrySelectorAll(".code");
+code[0].focus();
+codes.forEach((code, idx) => {
+	code.addEventListener("keydown",(e) => {
+		console.log(e.key);
+	})
+})
