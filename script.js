591f34b757accc2f5b306361bbf45aeb5496f3b9
pray63764 <141926153+pray63764@users.noreply.github.com>
2023-11-30 16:20:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const codes = document.querrySelectorAll(".code");
 code[0].focus();
 codes.forEach((code, idx) => {
 	code.addEventListener("keydown",(e) => {
-		console.log(e.key);
+		if(e.key >= 0 && e.key <=9){
+		     codes[idx+1].focus();	
+			setTimeout(() => codes[idx+1].focus(), 10);
+		}else if(e.key ==="Backspace"){
+			codes[idx-1].focus();
+		}
 	})
 })
